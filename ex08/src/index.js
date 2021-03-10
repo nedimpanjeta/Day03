192165c767a22ace8c942246603f8dfe09bd65dc
nedimpanjeta <panjeta.nedim@gmail.com>
2021-03-10 20:53:31 +0100

    add numb

---
+++ ex08/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+
+//Only change code below this line
+var sum;
+var numOne;
+var numTwo;
+
+int numOne = 15;
+int numTwo = 8;
+int sum = numOne + numTwo
+
+System.out.println(sum);
+
+return sum
+    
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main;
