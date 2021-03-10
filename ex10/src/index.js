26b71b36cebd7d61785389f1e53c4c66d79cf355
nedimpanjeta <panjeta.nedim@gmail.com>
2021-03-10 22:08:05 +0100

    product

---
+++ ex10/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne, numTwo) {
+
+//Only change code below this line
+var product;
+var numOne;
+var numTwo;
+
+int numOne = 15;
+int numTwo = 3;
+int product = numOne * numTwo
+
+numOne * numTwo = myRes
+
+System.out.println(product);
+
+return product;
+    
+}
+
+console.log(main(15, 3));
+module.exports = main;
