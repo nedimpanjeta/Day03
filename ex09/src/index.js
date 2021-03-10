c4ae4551d64dbc4e2ffa6453f58bcf0493a03554
nedimpanjeta <panjeta.nedim@gmail.com>
2021-03-10 21:26:28 +0100

    diff

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+
+//Only change code below this line
+var difference;
+var numOne;
+var numTwo;
+
+int numOne = 15;
+int numTwo = 8;
+int difference = numOne - numTwo
+
+System.out.println(sum);
+
+return difference;
+    
+}
+
+console.log(main(15, 8));
+module.exports = main;
